b0122cc26a1ea23c00491b29b5ff37c6d9a72ff1
Layomx <73512423+Layomx@users.noreply.github.com>
2024-10-23 10:54:11 -0500

    Script para borrar X cantidad de imagenes

---
+++ BorrarImagenes.py
@@ -0,0 +1,43 @@
+import os
+import random
+
+# Ruta principal que contiene las carpetas de letras (a, b, ..., z)
+carpeta_principal = 'C:/Users/drake/OneDrive/Documentos/Universidad/6) Tercer Año, Segundo Semestre/Hackaton/Dataset/Set-Original/Dataset'
+
+# Extensión de los archivos a procesar (puedes cambiarla si no son .png)
+extension = ".png"
+
+# Número máximo de imágenes permitido por subcarpeta (may o min)
+max_imagenes = 450
+
+# Recorremos cada carpeta de letras (a, b, ..., z)
+for letra in os.listdir(carpeta_principal):
+    ruta_letra = os.path.join(carpeta_principal, letra)
+
+    # Verificamos que sea una carpeta (la de la letra)
+    if os.path.isdir(ruta_letra):
+        # Recorremos las subcarpetas 'may' y 'min' dentro de cada letra
+        for subcarpeta in ['may', 'min']:
+            ruta_subcarpeta = os.path.join(ruta_letra, subcarpeta)
+
+            # Verificamos que la subcarpeta 'may' o 'min' existe
+            if os.path.exists(ruta_subcarpeta):
+                # Listamos todas las imágenes .png en la subcarpeta
+                imagenes = [f for f in os.listdir(ruta_subcarpeta) if f.endswith(extension)]
+                
+                # Si hay más imágenes de las necesarias, eliminamos al azar
+                if len(imagenes) > max_imagenes:
+                    exceso = len(imagenes) - max_imagenes
+                    # Elegimos al azar las imágenes a eliminar
+                    imagenes_a_eliminar = random.sample(imagenes, exceso)
+                    
+                    # Eliminamos las imágenes seleccionadas
+                    for imagen in imagenes_a_eliminar:
+                        ruta_imagen = os.path.join(ruta_subcarpeta, imagen)
+                        os.remove(ruta_imagen)
+                        print(f'Eliminada: {ruta_imagen}')
+
+                else:
+                    print(f'La carpeta {subcarpeta} en {letra} tiene {len(imagenes)} imágenes, no se necesita eliminar.')
+
+print("Proceso completado.")
