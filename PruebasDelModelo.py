4905b8f1bb9082cbe8b11762cdc25e3423e8ec45
Layomx <drakeadamslink@gmail.com>
2024-10-22 19:04:22 -0500

    Lista de clases

--- PruebasDelModelo.py
+++ PruebasDelModelo.py
@@ -1,3 +1,12 @@
 import os 
 import tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet_v2
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
+from PIL import Image
+import numpy as np 
+import tensorflow as tf 
+import cv2 
+
+# Lista de las clases
+categories = os.listdir('C:/Users/drake/OneDrive/Documentos/Universidad/6) Tercer Año, Segundo Semestre/Hackaton/Dataset/Set-Original/dataset_for_model/train')
+categories.sort
+print(categories)
