be3528f1c70b4074d8f276478d370c3594550db5
Layomx <drakeadamslink@gmail.com>
2024-10-22 19:49:32 -0500

    Pruebas

--- PruebasDelModelo.py
+++ PruebasDelModelo.py
@@ -13,16 +13,17 @@ print(categories)
 
 # Cargamos el modelo guardado
 path_for_saved_model = 'C:/Users/drake/OneDrive/Documentos/Universidad/6) Tercer Año, Segundo Semestre/Hackaton/Dataset/Set-Original/dataset_for_model/AlfabetoV2.h5'
-model = tf.keras.models.load_mode(path_for_saved_model)
+model = tf.keras.models.load_model(path_for_saved_model)
 
 print(model.summary())
 
 def classify_image(imageFile):
     x = []
 
-    img = Image.open(imageFile)
+    img = Image.open(imageFile).convert('L')
+    img = img.convert('RGB')
     img.load()
-    img = img.resize((224, 224), Image.ANTIALIAS)
+    img = img.resize((224, 224), Image.Resampling.LANCZOS)
 
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis = 0)
@@ -36,11 +37,11 @@ def classify_image(imageFile):
     categoryValue = categoryValue[0]
     print(categoryValue)
 
-    result = categories(categoryValue)
+    result = categories[categoryValue]
 
     return result
 
-imagePath = "C:/Users/drake/OneDrive/Documentos/Universidad/6) Tercer Año, Segundo Semestre/Hackaton/Dataset/dataset/train/Letter_A_may_404.png"
+imagePath = "C:/Users/drake/OneDrive/Documentos/Universidad/6) Tercer Año, Segundo Semestre/Hackaton/Dataset/dataset/train/Letter_s_min_872.png"
 resultText = classify_image(imagePath)
 print(resultText)
 
