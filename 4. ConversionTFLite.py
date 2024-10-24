8d70c29ea97e6c8145d36c54aa0140716a82fd0e
Layomx <73512423+Layomx@users.noreply.github.com>
2024-10-24 15:18:44 -0500

    Cambios en la documentacion

--- 4. ConversionTFLite.py
+++ 4. ConversionTFLite.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
+# Este codigo es basico y sencillo, su funcion es transformar el modelo h5 en tflite para transportarlo a Android
+
 # Se carga el modelo H5
 path_for_saved_model = 'C:/Users/drake/OneDrive/Documentos/Universidad/6) Tercer Año, Segundo Semestre/Hackaton/Dataset/Set-Original/dataset_for_model/Alfabeto25V2.h5'
 model = tf.keras.models.load_model(path_for_saved_model)
